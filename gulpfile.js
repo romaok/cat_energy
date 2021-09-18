e4131966cc6c1725c83c14aab0f3b817a6aed71c
Romaok <trojan2008@ukr.net>
2021-09-18 20:38:04 +0300

    Create

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const postcssUrl = require("postcss-url");
+const postcssImport = require("postcss-import");
+const postScss = require("postcss-scss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
+
+// Styles
+
+const styles = () => {
+  return gulp.src("source/sass/style.scss", { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(postcss([
+      postcssImport(),
+      postcssUrl(),
+    ], { syntax: postScss }))
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css", { sourcemaps: "." }))
+    .pipe(sync.stream());
+}
+
+exports.styles = styles;
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+exports.server = server;
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", sync.reload);
+}
+
+exports.default = gulp.series(
+  styles, server, watcher
+);
